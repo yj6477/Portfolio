7571d56fe389f62dd7a805e7336919e10ba09752
ys514111 <youngjin.seo.963@my.csun.edu>
2021-09-07 22:12:23 -0700

    Added navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {FaBars, FaLinkedinIn, FaGithub} from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <div>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/portfolio">
+                        YoungJin Seo
+                    </NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="projects">Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="contact">Contact</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks as="a" href="https://www.linkedin.com/in/youngjinseo127/" target="_blank"><FaLinkedinIn/></NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks as="a" href="https://github.com/yj6477" target="_blank"><FaGithub/></NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </div>
+    )
+}
+
+export default Navbar
